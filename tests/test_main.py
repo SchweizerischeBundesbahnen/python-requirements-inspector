593e2001bb3477068a1512ca5808fb3d41a331c5
Sebastian Schedegger <sebastian.scheidegger@sbb.ch>
2026-08-10 11:48:37 +0200

    fix: path injection vulnerability in main
    Fixed test_main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@
 
 import json
 import tempfile
+from contextlib import chdir
 from pathlib import Path
 
 from python_requirements_inspector import main
@@ -30,23 +31,23 @@ def test_main():
     ]
 
     # write test data to json file
-    with tempfile.NamedTemporaryFile(prefix="test_", suffix=".json", delete=False, mode="w+", encoding="utf-8") as input_json_file:
+    with tempfile.NamedTemporaryFile(prefix="test_", suffix=".json", delete=False, mode="w+", encoding="utf-8") as input_json_file, chdir(Path(input_json_file.name).parent):
         json.dump(test_data, input_json_file)
         input_json_file.flush()
 
-    # execute main with json file
-    output_file_path = main.main(input_json_file.name)
+        # execute main with json file
+        output_file_path = main.main(input_json_file.name)
 
-    # read output json file
-    with Path(output_file_path).open(encoding="utf-8") as output_json_file:
-        output_data = json.load(output_json_file)
+        # read output json file
+        with Path(output_file_path).open(encoding="utf-8") as output_json_file:
+            output_data = json.load(output_json_file)
 
-    # delete json files
-    Path(input_json_file.name).unlink()
-    Path(output_json_file.name).unlink()
+        # delete json files
+        Path(input_json_file.name).unlink()
+        Path(output_json_file.name).unlink()
 
-    expected_dataset_count = 2
+        expected_dataset_count = 2
 
-    # check if outfile has a entry per data set
-    assert len(output_data) == expected_dataset_count
-    assert "öüäß" in output_data[1]["smellDescription"]
+        # check if outfile has a entry per data set
+        assert len(output_data) == expected_dataset_count
+        assert "öüäß" in output_data[1]["smellDescription"]
